f23c210192701c674c27ef597b1c7982af3da182
kitaevdmitry <kitaevdmitry95@gmail.com>
2026-08-12 10:26:18 +0300

    обновили файлы  с админкой

--- app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { ADMIN_COOKIE, adminCookieOptions } from "../../../admin-auth";
-
-export async function GET(request: Request) {
-  const response = NextResponse.redirect(new URL("/", request.url), 303);
-  response.cookies.set(ADMIN_COOKIE, "", { ...adminCookieOptions, maxAge: 0 });
-  return response;
-}
